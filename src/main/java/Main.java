04979d98776d07b8b0d44440ba588791dc408908
amarcinkowski <andrzej.marcinkowski@gmail.com>
2017-02-26 20:12:14 +0100

    added spock and gep deps

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by amarcinkowski on 26.02.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("empty");
+    }
+
+    public static int max(int a, int b) {
+        return a > b ? a : b;
+    }
+}
