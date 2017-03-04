85a6c332a07e8beee015946d2840502ccb3527d0
amarcinkowski <andrzej.marcinkowski@gmail.com>
2017-03-04 10:29:07 +0100

    param not set exc

--- src/main/java/com/github/amarcinkowski/metro/DuplicateFunctionException.java
+++ src/main/java/com/github/amarcinkowski/metro/DuplicateFunctionException.java
@@ -1,7 +1,5 @@
 package com.github.amarcinkowski.metro;
 
-import groovy.transform.InheritConstructors;
-
 public class DuplicateFunctionException extends RuntimeException {
 
     public DuplicateFunctionException(String msg) {
---
+++ src/main/java/com/github/amarcinkowski/metro/ParamNotSetException.java
@@ -0,0 +1,8 @@
+package com.github.amarcinkowski.metro;
+
+public class ParamNotSetException extends RuntimeException {
+
+    public ParamNotSetException(String msg) {
+        super(msg);
+    }
+}
